4fc92fd2268d0ac1bd96ca33e4a3e88b4ef23f75
Zielony <niewartotupisac@gmail.com>
2020-01-21 00:42:27 +0100

    copied original swagger-parser code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SwaggerParserAndroid
 swagger-parser working on Android (Java 1.6)
+
+This is a fork of [https://github.com/swagger-api/swagger-parser](https://github.com/swagger-api/swagger-parser) working on Android. For information on how to use the library visit the original repository.
+
+Changes:
+
+ - slf4j replaced with plain Android logs
+ - streams replaced with this backport: https://github.com/aNNiMON/Lightweight-Stream-API
+ - nio replaced with this backport: https://github.com/lukhnos/nnio
+ 
---
+++ swaggerparsercore/src/main/java/io/swagger/v3/parser/core/models/ParseOptions.java
@@ -0,0 +1,54 @@
+package io.swagger.v3.parser.core.models;
+
+public class ParseOptions {
+    private boolean resolve;
+    private boolean resolveCombinators = true;
+    private boolean resolveFully;
+    private boolean flatten;
+    private boolean flattenComposedSchemas;
+    private boolean skipMatches;
+
+    public boolean isResolve() {
+        return resolve;
+    }
+
+    public void setResolve(boolean resolve) {
+        this.resolve = resolve;
+    }
+
+    public boolean isResolveCombinators() {
+        return resolveCombinators;
+    }
+
+    public void setResolveCombinators(boolean resolveCombinators) {
+        this.resolveCombinators = resolveCombinators;
+    }
+
+    public boolean isResolveFully() {
+        return resolveFully;
+    }
+
+    public void setResolveFully(boolean resolveFully) {
+        this.resolveFully = resolveFully;
+    }
+
+    public boolean isFlatten() { return flatten; }
+
+    public void setFlatten(boolean flatten) { this.flatten = flatten; }
+
+    public boolean isSkipMatches() {
+        return skipMatches;
+    }
+
+    public void setSkipMatches(boolean skipMatches) {
+        this.skipMatches = skipMatches;
+    }
+
+    public boolean isFlattenComposedSchemas() {
+        return flattenComposedSchemas;
+    }
+
+    public void setFlattenComposedSchemas(boolean flattenComposedSchemas) {
+        this.flattenComposedSchemas = flattenComposedSchemas;
+    }
+}
